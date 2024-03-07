831e2d634085a9f87f95ed8f9529f1e86d9a71a3
AshuSwym <ashutosh.singh@swymcorp.com>
2024-03-07 19:53:08 +0530

    [0.8] Add : Graph that shows request logs

--- client/component/Graph.jsx
+++ client/component/Graph.jsx
@@ -14,8 +14,9 @@ import { Line } from 'react-chartjs-2'
 ChartJS.register(TimeScale, LinearScale, PointElement, LineElement, Tooltip)
 ChartJS.defaults.borderColor = '#fff'
 ChartJS.defaults.color = '#fff'
-ChartJS.defaults.font.size = 14
-ChartJS.defaults.font.family = 'Helvetica'
+ChartJS.defaults.font.size = 12
+ChartJS.defaults.font.family = 'Jetbrains Mono'
+ChartJS.defaults.scale.grid.display = false
 
 const Graph = () => {
     const [data, setData] = useState({})
@@ -32,15 +33,13 @@ const Graph = () => {
             scales: {
                 x: {
                     type: 'time',
+                    color: "red",
+                    backgroundColor: "black",
+
                     time: {
                         displayFormats: {
                             quarter: 'MMM YYYY',
                         },
-                    },
-                    grid: {
-                        display: false,
-                        drawOnChartArea: false,
-                        drawTicks: false,
                     },
                 },
                 y: {
@@ -54,7 +53,7 @@ const Graph = () => {
     useEffect(() => {
         const getData = async () => {
             const dataLine = {}
-            const logs = await axios.get('logs/getLogs')
+            const logs = await axios.get('/logs/getLogs')
             logs.data.forEach((log) => {
                 const time = getDate(log.time)
                 const val = dataLine[time] ? dataLine[time] + 1 : 1
@@ -71,17 +70,18 @@ const Graph = () => {
 
     return (
         <div
-            className="rounded-md p-2 "
+            className="rounded-md"
             style={{
                 background: 'rgba( 13, 13, 13, 0 )',
-                'box-shadow': '0 8px 32px 0 rgba( 31, 38, 135, 0.37 )',
-                'backdrop-filter': 'blur( 13.5px )',
-                '-webkit-backdrop-filter': 'blur( 13.5px )',
-                'border-radius': '10px',
+                boxShadow: '0 8px 32px 0 rgba( 31, 38, 135, 0.37 )',
+                backdropFilter: 'blur( 13.5px )',
+                WebkitBackdropFilter: 'blur( 13.5px )',
+                borderRadius: '10px',
                 border: '1px solid rgba( 255, 255, 255, 0.18 )',
+                marginTop: "10px"
             }}
         >
-            <div className="bg-white-300 p-4 rounded-md  h-350-px">
+            <div className="bg-white-300 p-2 sm:p-4 rounded-md h-350-px">
                 <Line
                     updateMode="resize"
                     options={options}
